8693025f1f30523b45f4ac232712ff2d0580c337
joshua yap <30822255+joshavenue@users.noreply.github.com>
2017-09-06 14:09:33 +0800

    Update set_sample_1.py

--- set_sample_1.py
+++ set_sample_1.py
@@ -18,9 +18,28 @@ COURSES = {
                     "functions", "input"}
 }
 
-def covers(param):
-    returnable = []
-    for course in COURSES:
-        if COURSES[course] & param:
-            returnable.append(course)
-    return returnable
+def covers(topics):
+    new_list = []
+    for topic in topics:
+        for basics in COURSES:
+            if topic in COURSES[basics]:
+                new_list.append(basics)
+    return new_list
+
+# OK, let's create something a bit more refined. Create a new function named covers_all that takes a single set as an argument. Return the names of all of the courses, in a list, where all of the topics in the supplied set are covered.
+# For example, covers_all({"conditions", "input"}) would return ["Python Basics", "Ruby Basics"]. Java Basics and PHP Basics would be exclude because they don't include both of those topics.
+
+def covers_all(topics):
+    new_list = []
+    temp_list = []
+    for basics in COURSES:
+        for topic in topics:
+            if topic in COURSES[basics]:              
+                temp_list.append(topic)
+        if len(topics) == len(temp_list):
+            if basics not in new_list:
+                new_list.append(basics)
+                temp_list = []
+        else:
+            temp_list = []
+    return new_list
